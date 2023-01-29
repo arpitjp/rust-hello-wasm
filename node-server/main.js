1637813bd74f01ea466b0ffa2ef1b16edc235312
Arpit Jain <arpitjp@gmail.com>
2023-01-29 17:48:01 +0530

    fib

--- node-server/main.js
+++ node-server/main.js
@@ -1,3 +1,14 @@
-import { greet } from "rust-hello-wasm";
+import { greet, fib } from "rust-hello-wasm";
 
 document.getElementById("btn")?.addEventListener("click", () => greet("The Office"));
+
+const num = 41;
+
+console.time("fib-wasm");
+console.log("fib-wasm", fib(num));
+console.timeEnd("fib-wasm");
+
+const jfib = (n) => (n <= 1 ? n : jfib(n - 1) + jfib(n - 2));
+console.time("jfib");
+console.log("jfib", jfib(num));
+console.timeEnd("jfib");
